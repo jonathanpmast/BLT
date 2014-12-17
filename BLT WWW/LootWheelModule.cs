90ddd43256e56dfac9e7d7cb1a752188803d0f4d
jonathanpmast <jonathanpmast@hotmail.com>
2014-12-17 09:51:06 -0500

    Initial Upload

---
+++ BLT WWW/LootWheelModule.cs
@@ -0,0 +1,13 @@
+﻿namespace BLT.WWW
+{
+    using Nancy;
+    public class LootWheelModule : NancyModule
+    {
+        public LootWheelModule() {
+            Get["sk"] = _ =>
+            {
+                return View["sk/index.cshtml"];
+            };
+        }
+    }
+}
---
+++ BLT WWW/Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace BLT.WWW
+{
+    using Microsoft.Owin;
+    using Nancy.ViewEngines.Razor;
+    using Owin;
+    using System.Collections.Generic;   
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+
+    public class RazorConfig : IRazorConfiguration
+    {
+        public IEnumerable<string> GetAssemblyNames()
+        {
+            yield return "BLT.WWW";
+            yield return "BLT.Core";
+        }
+
+        public IEnumerable<string> GetDefaultNamespaces()
+        {
+            yield return "BLT.WWW";
+            yield return "BLT.Core.Models";
+        }
+
+        public bool AutoIncludeModelNamespace
+        {
+            get { return true; }
+        }
+    }
+}
