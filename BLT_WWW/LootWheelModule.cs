e5af6df1fdf03630cd623140374e328a91b86051
jonathanpmast <jonathanpmast@hotmail.com>
2014-12-21 18:39:11 -0500

    More good stuff

--- BLT_WWW/LootWheelModule.cs
+++ BLT_WWW/LootWheelModule.cs
@@ -1,13 +1,17 @@
 ﻿namespace BLT.WWW
 {
     using Nancy;
+    using BLT.Core.Models;
+using BLT.Core.Logging;
     public class LootWheelModule : NancyModule
     {
+        private readonly static ILog logger = LogProvider.GetCurrentClassLogger();
         public LootWheelModule() {
             Get["sk"] = _ =>
             {
-                
-                return View["sk/index.cshtml"];
+                logger.Debug("Get: Lootwheel (/sk)");
+                TestModel tm = new TestModel() { Message = System.Web.Configuration.WebConfigurationManager.AppSettings["TestKey"] };
+                return View["sk/index.cshtml",tm];
             };
         }
     }
--- BLT_WWW/Startup.cs
+++ BLT_WWW/Startup.cs
@@ -1,9 +1,14 @@
 ﻿namespace BLT.WWW
 {
     using Microsoft.Owin;
+    using Nancy;
+    using Nancy.Bootstrapper;
+    using Nancy.TinyIoc;
     using Nancy.ViewEngines.Razor;
     using Owin;
-    using System.Collections.Generic;   
+    using System.Collections.Generic;
+
+    using BLT.Core.Logging;
     public class Startup
     {
         public void Configuration(IAppBuilder app)
@@ -12,8 +17,24 @@
         }
     }
 
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        private readonly static ILog logger = LogProvider.GetCurrentClassLogger();
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            logger.Debug("Application Startup, Start");
+            base.ApplicationStartup(container, pipelines);
+            logger.Debug("Application Startup, Registering RazorConfig");
+            container.Register<IRazorConfiguration>(new RazorConfig());
+            
+            logger.Debug("Application Startup, End");
+        }
+    }
+
     public class RazorConfig : IRazorConfiguration
     {
+
         public IEnumerable<string> GetAssemblyNames()
         {
             yield return "BLT.WWW";
