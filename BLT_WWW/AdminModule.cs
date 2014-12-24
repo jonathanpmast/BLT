759446b39f576938ff2bb5300b20e7eaafa08359
jonathanpmast <jonathanpmast@hotmail.com>
2014-12-23 19:05:28 -0500

    More interim.

---
+++ BLT_WWW/AdminModule.cs
@@ -0,0 +1,47 @@
+﻿using BLT.Core.Import;
+using BLT.Core.Logging;
+using Nancy;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace BLT.WWW
+{
+    public class AdminModule :NancyModule
+    {
+        private readonly static ILog logger = LogProvider.GetCurrentClassLogger();
+        public AdminModule()
+            : base("admin")
+        {
+            Get["/ksk"] = _ =>
+            {
+                logger.Debug("Get: admin/ksk (/ksk)");
+                return View["admin/lootwheel.cshtml"];
+
+            };
+
+            Post["/ksk/upload"] = _ =>
+            {
+                logger.Debug("Post: ksk/upload (/ksk/upload)");
+                var file = this.Request.Files.FirstOrDefault();
+                string contents = string.Empty;
+                if (file == null)
+                {
+                    throw new Exception("You didn't upload a file! I probably should catch this exception earlier!");
+                }
+                using (StreamReader sr = new StreamReader(file.Value))
+                {
+                    logger.Debug("Post: ksk/upload: reading file contents");
+                    contents = sr.ReadToEnd();
+                    logger.Debug("Post: ksk/upload: succesfully read file contents");
+                }
+                logger.Debug("Post: ksk/upload: parsing XML with ListImportResult class");
+                KSKListImportResult results = KSKListImportResult.Load(contents);
+                logger.Debug("Post: ksk/upload: (ksk/upload) done");
+                return View["admin/lootwheel_upload.cshtml", results];
+            };
+        }
+    }
+}
--- BLT_WWW/LootWheelModule.cs
+++ BLT_WWW/LootWheelModule.cs
@@ -20,33 +20,7 @@
                 return View["sk/index.cshtml", tm];
             };
 
-            Get["/admin/ksk"] = _ =>
-            {                
-                logger.Debug("Get: admin (/admin)");
-                return View["sk/admin.cshtml"];
-                
-            };
-
-            Post["/admin/upload"] = _ =>
-            {
-                logger.Debug("Post: upload (/admin/upload)");
-                var file = this.Request.Files.FirstOrDefault();
-                string contents = string.Empty;
-                if (file == null)
-                {
-                    throw new Exception("You didn't upload a file! I probably should catch this exception earlier!");
-                }
-                using (StreamReader sr = new StreamReader(file.Value))
-                {
-                    logger.Debug("Post: upload: reading file contents");
-                    contents = sr.ReadToEnd();
-                    logger.Debug("Post: upload: succesfully read file contents");
-                }
-                logger.Debug("Post: upload: parsing XML with ListImportResult class");
-                KSKListImportResult results = KSKListImportResult.Load(contents);
-                logger.Debug("Post: upload: (/admin/upload) done");
-                return View["sk/upload_results.cshtml", results];
-            };
+            
         }
     }
 }
