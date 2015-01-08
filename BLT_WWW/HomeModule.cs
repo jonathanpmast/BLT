f0aea0b9f6531989be04cfa1dfd87d271cd0fb80
jonathanpmast <jonathanpmast@hotmail.com>
2015-01-08 01:37:24 -0500

    Testing CI

---
+++ BLT_WWW/HomeModule.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BLT.WWW
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ =>
+            {
+                return View["Home/index.cshtml"];
+            };
+        }
+    }
+}
