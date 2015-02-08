20d4db547133247fc1893669033ec4e053f61720
jonathanpmast <jonathanpmast@hotmail.com>
2015-02-08 00:45:53 -0500

    Converted to .less files and a gupl build for the less

---
+++ BLT_WWW/gulpfile.js
@@ -0,0 +1,21 @@
+﻿var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    plugins = require('gulp-load-plugins')(),
+    handleErrors = require('./build/handleErrors.js');
+;
+
+gulp.task('default', ['watch']);
+
+gulp.task('less', function () {
+    return gulp.src('less/app.less')
+            .pipe(plugins.sourcemaps.init())
+            .pipe(plugins.less())
+            .on('error', handleErrors)
+            .pipe(plugins.sourcemaps.write())
+            .pipe(gulp.dest('./Content'));
+
+});
+
+gulp.task('watch', ['less'], function () {
+    gulp.watch('less/*.less', ['less']);
+});
